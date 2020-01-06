d66aee9541901b5be16993e5e1829cc41874f74f
ValentinaPD <valya.ponomarenko.00@mail.ru>
2020-01-07 02:29:57 +0700

    Создание Encrypt и Decrypt

--- ConsoleApplication13/Program.cs
+++ ConsoleApplication13/Program.cs
@@ -6,22 +6,21 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication13
 {
+    
+    
     class Program
     {
+       
         public static int sundaram(int n)
         {
             int[] a = new int[n];
             int i, j, k;
             for (i = 1; 3 * i + 1 < n; i++)
-            {
                 for (j = 1; (k = i + j + 2 * i * j) < n && j <= i; j++)
                     a[k] = 1;
-            }
             for (i = n - 1; i >= 1; i--)
                 if (a[i] == 0)
-                {
                     return (2 * i + 1);
-                }
             return 3;
         }
 
@@ -37,12 +36,7 @@ namespace ConsoleApplication13
             return Math.Abs(a);
         }
 
-
-
-
-
-
-        public static char GetSymbolNumber(double number)//symbol
+        public static char GetSymbolNumber(double number)
         {
             char[] symbols = new char[] { '.', ',', ':',';','#','%','*','/','(',')', '[', ']', '{', '}', '&', '@', '!', '?', '<', '>' };
             char[] nums = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
@@ -61,17 +55,9 @@ namespace ConsoleApplication13
                 i++;
             }
             for (int j =0; j<symbols.Length; j++)
-            {
-                
                 if (j+60 == number) return symbols[j];
-                //i++;
-            }
             for (int j = 0; j < nums.Length; j++)
-            {
-
                 if (j + 80 == number) return nums[j];
-                //i++;
-            }
             if (number == 0) return ' ';
             return letter;
         }
@@ -83,28 +69,20 @@ namespace ConsoleApplication13
             int i = 0;
             for (int symbol = 'а'; symbol <= 'я'; ++symbol)
             {
-
                 letter = (char)symbol;
-                if (letter == s)  { Console.WriteLine("Russian" + (i + 1));return i + 1; };
+                if (letter == s) return i + 1; 
                 i++;
             }
             for (int symbol = 'a'; symbol <= 'z'; ++symbol)
             {
-
                 letter = (char)symbol;
-                if (letter == s) { Console.WriteLine("English" + (i + 1)); return i + 1; };
+                if (letter == s)  return i + 1; 
                 i++;
             }
             for (int j=0;j<symbols.Length; j++)
-            {
-
                 if (s == symbols[j]) return j + 60;
-            }
             for (int j = 0; j < nums.Length; j++)
-            {
-
                 if (s == nums[j]) return j + 80;
-            }
             if (s == ' ') return 0;
             return i;
         }
@@ -124,25 +102,66 @@ namespace ConsoleApplication13
         {
             Random rand = new Random();
             int d = 0;
-            do
-            {
-                d = rand.Next() % 100;
-                
-            } while (nod(d, fn) != 1);
+             do         
+                d = rand.Next() % 100;                
+             while (nod(d, fn) != 1);
             return d;
         }
         public static int GetE(int fn,int d)
         {
             int e = 0;
             do
-            {
-                e += 1;
-                
-            } while ((e * d) % (fn)!=1);
-            //Console.WriteLine("e=" + e);
+                e += 1;              
+            while ((e * d) % (fn)!=1);
+            
             return e;
         }
-       
+        public static int[] Encrypt(string str,int e,int n)
+        {
+           
+            int[] CryptoText = new int[str.Length];
+            int b = 301;
+            int c;
+            for (int j = 0; j < str.Length; j++)
+            {
+                c = 1;
+                long i = 0;
+                int ASCIIcode = GetNumberSymbol(str[j]) + b;
+                while (i < e)
+                {
+                    c = c * ASCIIcode;
+                    c = c % n;
+                    i++;
+                }
+                CryptoText[j] = c;
+                
+                b += 1;
+            }
+            return CryptoText;
+        }
+        public static string Decrypt(int[] CryptoText,int d,int n)
+        {
+            int[] Tdecrypt = new int[CryptoText.Length];
+            int b = 301;
+            int m;
+            string str = "";
+            for (int j = 0; j < CryptoText.Length; j++)
+            {
+                m = 1;
+                long i = 0;
+                while (i < d)
+                {
+                    m = m * CryptoText[j];
+                    m = m % n;
+                    i++;
+                }
+                m = m - b;
+                Tdecrypt[j] = m;
+                b += 1;
+                str+=GetSymbolNumber(Tdecrypt[j]);
+            }
+            return str;
+        }
         static void Main(string[] args)
         {
             Random rand = new Random();
@@ -168,46 +187,11 @@ namespace ConsoleApplication13
             int e = GetE(fn, d);
 
             string Text = "i love you c#, но php тоже не плох!";
-            int MAX = Text.Length;
-            int[]CryptoText = new int[MAX];
-            int[] Tdecrypt = new int[MAX];
-            int b = 301;
-            int c;
-            for (int j = 0; j < MAX; j++)
-            {
-                c = 1;
-                long i = 0;
-                int ASCIIcode = GetNumberSymbol(Text[j])+b;
-                while (i < e)
-                {
-                    c = c * ASCIIcode;
-                    c = c % n;
-                    i++;
-                }
-                CryptoText[j] = c;
-               Console.WriteLine(CryptoText[j]);
-                b += 1;
-            }
 
-
-///////////////////////////////////////////////////
-            b = 301;
-            int m;
-            for (int j = 0; j < MAX; j++)
-            {
-                m = 1;
-                long i = 0;
-                while (i < d)
-                {
-                    m = m * CryptoText[j];
-                    m = m % n;
-                    i++;
-                }
-                m = m - b;
-                Tdecrypt[j] = m;
-                b += 1;
-                Console.WriteLine(GetSymbolNumber(Tdecrypt[j]));
-            }
+            string str = Decrypt(Encrypt(Text, e, n), d, n);
+            Console.WriteLine(str);
+            
+           
           
         }
         
